d4aee3ae77cf8bcdea37d97aecc1ca6f70f424ee
Paul Wood <paul.wood@payaut.com>
2022-05-08 17:27:50 +0200

    Add service mock keycloack
    Mock Keycloak service will be responsible for keeping track of user information within the system, its just a mock though ideally I would have liked to set up a keycloak instance.

---
+++ keycloak-mock/src/main/java/com/getir/keycloakmock/KeycloackMockApplication.java
@@ -0,0 +1,13 @@
+package com.getir.keycloakmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloackMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloackMockApplication.class, args);
+    }
+
+}
---
+++ keycloak-mock/src/test/java/com/getir/keycloakmock/KeycloackMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getir.keycloakmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
